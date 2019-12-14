fa23ca0fde027cdc50f42f791949ed09f80a2801
zhangluzhanglu <1758658975@qq.com>
2019-12-14 14:07:18 +0800

    优化了下使用体验

--- README.md
+++ README.md
@@ -1,29 +1,48 @@
-# 此模块用于获取微信公众号调用js-sdk时的配置对象
+#### 作用
 
-   * 函数名：getConfigObj
-   * 作用：根据传入的参数获取权限验证配置对象
-   * 参数：baseObj,res
-   * baseObj:表示公众号配置信息的基础对象
-      {
-        url:"",//使用此对象的前端网页的URL
-        appid:"",//微信公众号的appid
-        secret:"",微信公众号的开发者密码
-      }
-   * res:
-      此次前端请的响应对象,用于把配置对象返回给前端的.
-      如果此参数没有传，那么就把配置对象作为函数返回值返回出去
+此模块用于获取微信公众号调用js-sdk时的配置对象
 
-# 使用示例
+#### 安装命令
 ``` javascript
+ cnpm i wxgzh-getconfigobj -S
+```
+#### 使用方式一 : 将配置对象作为返回值返回
+``` javascript
+//引入此模块
+var wxgzh_getConfigObj=require("wxgzh-getconfigobj");
+
 //获取调用js-sdk时需要的配置对象
+var configObj = wxgzh_getConfigObj(
+  {
+    url: "http://zlabcd.vaiwan.com/",//使用此对象的前端网页的URL
+    appid: "wxfe0******07d707",//微信公众号的appid
+    secret: "a8b5******091a1c727",//微信公众号的开发者密码
+  },
+  null,//是否将配置对象响应到前端，null表示返回不响应，只作为返回值返回
+  true,//是否打印出日志，true表示打印，默认为true
+)
+
+//将返回的配置对象显示出来
+configObj.then(function (val) {
+  console.log("configObj:\n", val)
+})
+```
+#### 使用方式二 : 将配置对象直接响应到前端
+``` javascript
+//引入此模块
+var wxgzh_getConfigObj=require("wxgzh-getconfigobj");
+
+//定义路由接口，直接将配置对象返回到前端
 router.get('/getConfigObj', function (req, res, next) {
-  wxgzh_getconfigobj(
+ wxgzh_getconfigobj(//获取调用js-sdk时需要的配置对象
     {
-      url: "http://zlabcd.****.com/",//使用此对象的前端网页的URL
-      appid: "******",//微信公众号的appid
-      secret: "********",//微信公众号的开发者密码
+      url: "http://zlabcd.vaiwan.com/",//使用此对象的前端网页的URL
+      appid: "wxfe0******07d707",//微信公众号的appid
+      secret: "a8b5******091a1c727",//微信公众号的开发者密码
     },
-    res,//传入响应对象
+    res,//传入响应对象！！！！
+    false,//生成配置对象的过程中不打印日志
   )
 });
+
 ```
--- src/wxgzh-getconfigobj.js
+++ src/wxgzh-getconfigobj.js
@@ -2,19 +2,18 @@ var sha1 = require('sha1');
 var request = require("request");
 
 /**
- * 函数名：getConfigObj
- * 作用：根据传入的参数获取权限验证配置对象
- * 参数：baseObj,res
- * baseObj:表示公众号配置信息的基础对象
-    {
-      url:"",//使用此对象的前端网页的URL
-      appid:"",//微信公众号的appid
-      secret:"",微信公众号的开发者密码
-    }
- * res:此次前端请的响应对象,用于把配置对象返回给前端的
- */
-
-function wxgzh_getConfigObj(baseObj, res) {
+此方法作用：将根据传入的参数获取权限验证配置对象
+*
+@param {Object} baseObj 公众号配置信息的基础对象
+*
+@param {Obejct} res 响应对象response。如果不传，则将返回含有配置对象的promise对象
+*
+@param {boolean} [isPrint=true] 日志打印控制，false表示不打印，true表示打印
+*
+@return {Promise} 如果传了第二个参数将不会有返回值，配置对象会直接响应到前端，如果没传将返回含有配置对象的promise
+*
+*/
+function wxgzh_getConfigObj(baseObj, res, isPrint=true) {
   if (!baseObj) {  //如果没有传入baseObj，则给一个默认值
     baseObj = {
       url: "",//使用此对象的前端网页的URL
@@ -43,15 +42,21 @@ function wxgzh_getConfigObj(baseObj, res) {
     var currentTime = new Date().getTime() / 1000;
     if (access_token === "" || currentTime - getAccessTokenTime >= 6900) {  //大于6900表示在两个小时前5分钟重新获取token
       request.get("https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=" + baseObj.appid + "&secret=" + baseObj.secret, function (err, response, body) {
-        console.info("response:", response.body);
         access_token = JSON.parse(response.body).access_token;//默认是字符串，需要手动转换下
         getAccessTokenTime = new Date().getTime() / 1000; //设置此时的时间，单位为秒
-        // console.info("access_token1:", access_token);
+        if (isPrint === true) {
+          console.info("----------------------------------------------------");
+          console.info("access_token:\n", access_token);
+          console.info("----------------------------------------------------");
+        }
         resolve(access_token)
       });
     }
     else {
-      // console.info("access_token2:", access_token);
+      if (isPrint === true) {
+        console.info("access_token:\n", access_token);
+        console.info("----------------------------------------------------");
+      }
       resolve(access_token)
     }
   })
@@ -63,18 +68,24 @@ function wxgzh_getConfigObj(baseObj, res) {
         request.get("https://api.weixin.qq.com/cgi-bin/ticket/getticket?access_token=" + d + "&type=jsapi", function (err, response, body) {
           var jsapi_ticket = JSON.parse(response.body).ticket
           getJsapi_ticket = new Date().getTime() / 1000; //设置此时的时间，单位为秒
-          // console.info("ticket1:", jsapi_ticket);
+          if (isPrint === true) {
+            console.info("jsapi_ticket:\n", jsapi_ticket);
+            console.info("----------------------------------------------------");
+          }
           resolve(jsapi_ticket);
         });
       })
     }
     else {
-      // console.info("ticket2:", jsapi_ticket);
+      if (isPrint === true) {
+        console.info("jsapi_ticket:\n", jsapi_ticket);
+        console.info("----------------------------------------------------");
+      }
       return jsapi_ticket;
     }
   })
   //--------根据jsapi_ticket，noncestr，timestamp，url生成签名--------------
-  rq2.then(function (d) {
+  return rq2.then(function (d) {
     if (d !== undefined) {
       configObj.jsapi_ticket = d;
       //先对所有待签名参数按照字段名的ASCII 码从小到大排序（字典序）后,使用URL键值对的格式（即key1=value1&key2=value2…）拼接成字符串string1
@@ -83,17 +94,24 @@ function wxgzh_getConfigObj(baseObj, res) {
         str1 += key + "=" + configObj[key] + "&"
       }
       var str1 = str1.slice(0, -1);
-      console.log("str1", str1)
+      if (isPrint === true) {
+        console.info("URL键值对:\n", str1);
+        console.info("----------------------------------------------------");
+      }
       //对string1进行sha1签名，得到 signature
       let signature = sha1(str1);
       configObj.signature = signature;
       //打印最后的configObj对象
-      console.log("configObj", configObj)
-      if(res) res.send({ ...configObj, appId: baseObj.appid, })
-      else return { ...configObj, appId: baseObj.appid}
+      if (isPrint === true) {
+        console.info("configObj:\n", configObj);
+        console.info("----------------------------------------------------");
+      }
+      if (res) res.send({ ...configObj, appId: baseObj.appid, })
+      else return { ...configObj, appId: baseObj.appid }
     }
   })
 
 }
 
 module.exports = wxgzh_getConfigObj;
+
